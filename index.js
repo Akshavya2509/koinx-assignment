b7b2d4c1c95de84713e6cb7ed9979434a962e0b3
Akshavya2509 <Akshavya2509@gmail.com>
2024-05-03 19:22:52 +0530

    created ethereum price checking function

--- index.js
+++ index.js
@@ -1,12 +1,28 @@
 const express = require("express");
 const app = express();
 const axios = require("axios");
-const Transaction = require("./config").default;
+const Transaction = require("./config");
 const PORT = process.env.PORT || 3000;
-
-// Define a MongoDB schema and model for transactions
-const apiUrl = `https://api.etherscan.io/api?module=account&action=balance&address=${ethereumAddress}&tag=latest&apikey=${apiKey}`;
+const Ethereum = require("./config");
+const cron = require("node-cron");
 // Define route to fetch transactions
+
+async function fetchAndStoreEthereumPrice() {
+  try {
+    const response = await axios.get(
+      "https://api.coingecko.com/api/v3/simple/price?ids=ethereum&vs_currencies=inr"
+    );
+    const ethereumPriceInINR = response.data.ethereum.inr;
+
+    const ethereumPriceEntry = new EthereumPrice({ price: ethereumPriceInINR });
+    await ethereumPriceEntry.save();
+
+    console.log(`Ethereum price fetched and stored: ${ethereumPriceInINR} INR`);
+  } catch (error) {
+    console.error("Error fetching Ethereum price:", error);
+  }
+}
+
 app.get("/transactions", async (req, res) => {
   const address = "0xce94e5621a5f7068253c42558c147480f38b5e0d";
   const apiKey = "AR1VJYIGTS9H9A2PZPN847H9PMD55FN4FU"; // Your Etherscan API key
@@ -21,13 +37,14 @@ app.get("/transactions", async (req, res) => {
     await Transaction.insertMany(transactions);
 
     res.json(transactions);
+
+    cron.schedule("*/10 * * * *", fetchAndStoreEthereumPrice);
   } catch (error) {
     console.error("Error fetching transactions:", error);
     res.status(500).json({ error: "Failed to fetch transactions" });
   }
 });
 
-// Start the server
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
